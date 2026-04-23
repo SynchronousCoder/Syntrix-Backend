c30cbfd49ef9e4cb15976119a672986d15a58296
Aryan Tyagi <dontfollowmann@gmail.com>
2026-04-23 22:35:26 +0530

    Changed port

--- app.js
+++ app.js
@@ -57,6 +57,6 @@ app.post("/data", async (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`✅ Backend running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`✅ Backend running on ${PORT}`);
 });
